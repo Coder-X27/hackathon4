f388c5bf45a90d277744be45a2e8bb8eab31d0c5
Karan <sharma.karan27oct@gmail.com>
2022-03-26 05:08:29 +0530

    added tabular form

--- src/Components/aditem.js
+++ src/Components/aditem.js
@@ -1,29 +1,14 @@
-
-
-import React from 'react';
-import Proptypes from 'prop-types';
-
-
- const Aditem = (props) => {
-
-
-    const { production, month, type, exports, capacity } = props;
-    return (
-
-        <div className='Aditem'>
-            <div>production={production}</div>
-            <div>month={month}</div>
-            <div>type:{type}</div>
-            <div>exports:{exports}</div>
-            <div>capacity:{capacity}</div>
-
-        </div>
-
-
-    );
+// import React from "react";
+// import Proptypes from "prop-types";
 
 
 
-}
-export default Aditem;
-
+// const Aditem = (props) => {
+//   const { production, month, type, exports, capacity } = props;
+//   return (
+//     <div className="Aditem">
+      
+//     </div>
+//   );
+// };
+// export default Aditem;
--- src/Components/admin.js
+++ src/Components/admin.js
@@ -1,58 +1,73 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Aditem from "./aditem";
 import { MenuItems } from "../mainpage/Navbar/MenuItems";
 import axios from "axios";
-
+import Navbar from '../mainpage/Navbar/Navbar'
+import BarChart from '../Components/BarChart'
+import PieChart from '../Components/PieChart'
+import "./admin.css";
 
 export const Admin = () => {
-
-const [data, setdata] = useState('');
-
-
-
-
+  const [data, setdata] = useState("");
 
   const getData = async () => {
     const res = await axios.get(`http://localhost:9004/api`);
     setdata(res.data);
-    
-    
   };
 
-
   useEffect(() => {
+    getData();
+  }, []);
   
-getData()
-   
-  }, [])
-
-  
- 
-
   return (
-    <div >
+    <div className="container3">
+      <Navbar/>
+      <div className="container12">
+        <div className="butns">
+          <button>Statistical Form</button>
+          <button>Pictorial Form</button>
+        </div>
+        {/* <div className="charts">
+          <div className="barchart margincharts" style={{ width: 700 }}>
+            <BarChart chartData={'Aditem'} />
+          </div>
+          <hr />
+          <div className="piechart margincharts" style={{ width: 400 }}>
+            <PieChart chartData={Aditem} /> */}
+          {/* </div>
+        </div> */}
+        <table class="table">
+          <thead>
+            <tr>
+              <th className="lwid" scope="col">Name</th>
+              <th className="swid" scope="col">Month</th>
+              <th className="swid" scope="col">Capacity</th>
+              <th className="swid" scope="col">Production</th>
+              <th className="swid" scope="col">Export</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+
       {data &&
         data.map((entry) => (
-          <Aditem
-            production={entry.production}
-            month={entry.month}
-            type={entry.type}
-            exports={entry.exports}
-            capacity={entry.capacity}
-          />
+        <>
+          <div className="container12">
+            <table className="table hr">
+              <tbody>
+                <tr>
+                  <td className="lwid">{entry.type}</td>
+                  <td className="swid">{entry.month}</td>
+                  <td className="swid">{entry.capacity}</td>
+                  <td className="swid">{entry.production}</td>
+                  <td className="swid">{entry.exports}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </>
         ))}
     </div>
   );
 };
-
-
-
-
-
-
-
-
-
-
-
